64f40f0ba35c9297c57b87b0e406b8612e6dfb1b
gaufung <gaufung@outlook.com>
2020-08-06 20:57:54 +0800

    add scatter

---
+++ Tindo.Blazor/Tindo.Blazor.ECharts/Options/Series/Scatter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tindo.Blazor.ECharts.Options.Series
+{
+    public class Scatter : SeriesBase
+    {
+        public override string Type => nameof(Scatter).ToLower();
+
+        public int? GeoIndex { get; set; }
+
+        public int? CalendarIndex { get; set; }
+
+        public bool? Large { get; set; }
+
+        public int? LargeThreshold { get; set; }
+
+        public Emphasis Emphasis { get; set; }
+
+        public double? Progressive { get; set; }
+
+        public int? ProgressiveThreshold { get; set; }
+
+    }
+}
